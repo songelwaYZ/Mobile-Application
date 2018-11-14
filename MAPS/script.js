0861cc6122fdce2ae07a2d0db3379d8bd0bf7c2f
Yamkela <songelwa.yz@gmail.com>
2018-11-14 15:53:43 +0200

    map

--- MAPS/script.js
+++ MAPS/script.js
@@ -1,33 +1,38 @@
+
 var appID= '1WB7LqCQdbOdyLygyQr3'
 var appCode = '1bV2IzGcpvox50hw2406gg'
 
 
-var url = "http://autocomplete.geocoder.api.here.com/6.2/suggest.json" +
-    "?app_id=" +appID+
-    " &app_code=" + appCode +
-    "&query="
 
-var geocodeUrl = "https://geocoder.api.here.com/6.2/geocode.json" + "?app_id=" +appID+ " &app_code=" + appCode + "&seacrhtext="
+var url = "http://autocomplete.geocoder.api.here.com/6.2/suggest.json" + "?app_id=" +appID + " &app_code=" + appCode + "&query="
+
+var geocodeUrl = "https://geocoder.api.here.com/6.2/geocode.json" 
+        + "?app_id=" + appID
+        + " &app_code=" + appCode 
+        + "&searchtext="
 
 
 var app = new Vue({
     el: '#app',
     data: {
         address: '',
-        results: []
+        results: [],
+        geocodeResults: []
     },
     methods:{
+        klick: function(result){
+            this.address = result.label
+        },
 
         find: function(){
             var _this = this
-            fetch(geocodeUrl+this.address)
-            .then(function (response){
+            fetch(geocodeUrl + this.address)
+            .then(function (response){    
                 return response.json()
-
             })
             .then(function(response){
-                console.log('geocode', response)
-                console.log('location', response.Response.View[0],Result[0].Location.DisplayPosition)
+                console.log('geocode' , response)
+                console.log('location', response.Response.View[0].Result[0].Location.DisplayPosition)
                 _this.geocodeResults = response.Response.View[0].Result
 
             })
@@ -48,12 +53,6 @@ var app = new Vue({
             }else{
                 console.log('must use a valid address')
             }
-            
-        },
-        Klick:function(result){
-            //this.address
-
         }
     }
 })
-    
